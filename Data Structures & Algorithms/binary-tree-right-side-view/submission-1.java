a479171b1961a549e0a809d56772b1eec2d71f05
Guruprasad Limbikai <107348172+Guru7348@users.noreply.github.com>
2026-06-12 22:49:02 +0530

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        List<List<Integer>> levels = levelOrder(root);
+        for(List<Integer> level:levels){
+            result.add(level.get(level.size()-1));
+        }
+        return result;
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        List<TreeNode> currentLevel = new ArrayList<>();
+        currentLevel.add(root);
+
+        traverse(currentLevel, result);
+
+        return result;
+    }
+
+    private void traverse(List<TreeNode> nodes,
+                          List<List<Integer>> result) {
+
+        if (nodes.isEmpty()) {
+            return;
+        }
+
+        List<Integer> level = new ArrayList<>();
+        List<TreeNode> nextLevel = new ArrayList<>();
+
+        for (TreeNode node : nodes) {
+
+            level.add(node.val);
+
+            if (node.left != null) {
+                nextLevel.add(node.left);
+            }
+
+            if (node.right != null) {
+                nextLevel.add(node.right);
+            }
+        }
+
+        result.add(level);
+
+        traverse(nextLevel, result);
+    }
+
+}
